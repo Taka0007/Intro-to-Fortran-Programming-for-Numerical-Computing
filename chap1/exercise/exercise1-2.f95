aa2fd7ea8b0303c292fb0bce3633731605df10f0
Taka <103984069+Taka0007@users.noreply.github.com>
2024-01-14 13:20:39 +0900

    Create exercise1-2.f95

---
+++ chap1/exercise/exercise1-2.f95
@@ -0,0 +1,19 @@
+Program ex1_2
+    Implicit None
+    Integer i,ans1,ans2,ans3
+    
+    ans1 = 0
+    ans2 = 0
+    ans3 = 0
+    
+    Do i = 1,100
+        ans1 = ans1 + i
+        ans2 = ans2 + i**2
+        ans3 = ans3 + i**3
+    END Do
+
+    Write(*,*) 'ans1=',ans1
+    Write(*,*) 'ans2=',ans2
+    Write(*,*) 'ans3=',ans3
+
+END Program ex1_2
